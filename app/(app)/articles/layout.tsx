6b5d03f15c52f6be513e9297f90c774024dd057a
sergienko-anastasiya <vt211_sas@student.ztu.edu.ua>
2024-05-14 21:40:09 +0300

    ƒeat: add chackra

--- app/(app)/articles/layout.tsx
+++ app/(app)/articles/layout.tsx
@@ -1,6 +1,6 @@
 'use client'
 import {ReactNode} from "react";
-import Link from "next/link";
+import { Link } from '@chakra-ui/next-js'
 import s from '@/css/header.module.scss'
 
 interface Props {
@@ -11,9 +11,9 @@ function ArticlesLayout({children}: Props) {
     return (
         <div className="mobile:bg-amber-50 desktop:bg-sky-50 tablet:bg-emerald-50">
             <header className={s.header}>
-                <Link href="/articles">Articles</Link>
-                <Link href="/articles/favorite">Articles favorite</Link>
-                <Link href="/articles/create">Articles create</Link>
+                <Link color='blue.400' _hover={{ color: 'blue.500' }} href="/articles">Articles</Link>
+                <Link color='blue.400' _hover={{ color: 'green.500' }} href="/articles/favorite">Articles favorite</Link>
+                <Link color='blue.400' _hover={{ color: 'red.500' }} href="/articles/create">Articles create</Link>
             </header>
             {children}
         </div>
