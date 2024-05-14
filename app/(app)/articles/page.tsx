3bafdaa7e32cdd3f36fb43b2e1b641df895cc1b7
sergienko-anastasiya <vt211_sas@student.ztu.edu.ua>
2024-05-14 20:06:09 +0300

    ƒeat: 3.2 completed

--- app/(app)/articles/page.tsx
+++ app/(app)/articles/page.tsx
@@ -1,7 +1,17 @@
-export default function Page() {
+import {api} from "@/utils";
+import { Suspense } from "react";
+import Loading from "@/app/(app)/articles/loading";
+
+export default async function Page() {
+
+    const posts = await api('/posts').then(({data}) => data);
+
     return (
         <div>
             Articles
+            <Suspense fallback={<Loading/>}>
+                {JSON.stringify(posts)}
+            </Suspense>
         </div>
     )
 }
